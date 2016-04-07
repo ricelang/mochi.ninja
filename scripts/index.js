b76c123be15bf9584bf62c07c0d127cd5f36293f
Vinh <kurei@axcoto.com>
2016-04-06 20:57:39 -0700

    es6

--- scripts/index.js
+++ scripts/index.js
@@ -1,61 +1,53 @@
-var sys = require('sys')
-var exec = require('child_process').exec
-var child
+const sys = require('sys')
+const exec = require('child_process').exec
+const child
 
-var TelegramBot = require('node-telegram-bot-api');
+const TelegramBot = require('node-telegram-bot-api');
 
-var token = process.env.TELEGRAM_TOKEN
-var deployCmd = process.env.DEPLOY_CMD
+const token = process.env.TELEGRAM_TOKEN
+const deployCmd = process.env.DEPLOY_CMD
 
 // Setup polling way
-var bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, {polling: true});
 
 // Matches /deploy [whatever]
-bot.onText(/\/run (.+)/, function (msg, match) {
-  console.log(msg)
-  //var fromId = msg.from.id;
-  var fromId = msg.chat.id;
-  var resp = match[1];
-  console.log(resp)
+bot.onText(/\/run (.+)/, (msg, match) => {
+  //const fromId = msg.from.id;
+  const fromId = msg.chat.id
+  const resp = match[1]
 
   if (isNaN(resp)) {
     resp = 900
   }
-  bot.sendMessage(fromId, "Daddy, I will ping you after " + resp + " seconds");
-  setTimeout(function() {
+
+  bot.sendMessage(fromId, "Daddy, I will ping you after " + resp + " seconds")
+  setTimeout( () => {
     bot.sendMessage(fromId, "Daddy, let's come home and feed me");
   }, resp * 1000)
-
-});
+})
 
 // Matches /deploy [whatever]
-bot.onText(/\/deploy (.+)/, function (msg, match) {
-  console.log(msg)
-  //var fromId = msg.from.id;
-  var fromId = msg.chat.id;
-  var resp = match[1];
-  console.log(msg)
+bot.onText(/\/deploy (.+)/, (msg, match) => {
+  //const fromId = msg.from.id;
+  const fromId = msg.chat.id
+  const resp = match[1]
 
   const child = exec(deployCmd,
            (error, stdout, stderr) => {
-             console.log(`stdout: ${stdout}`);
-             console.log(`stderr: ${stderr}`);
              if (error !== null) {
-               console.log(`exec error: ${error}`);
+               console.log(`exec error: ${error}`)
              }
-             bot.sendMessage(fromId, `Daddy, Deployment is done. \n ${stderr} ${stdout}`);
+             bot.sendMessage(fromId, `Daddy, Deployment is done. \n ${stderr} ${stdout}`)
            })
-  bot.sendMessage(fromId, "Daddy, I'm sleeping, will deploy in 5 mins");
-});
+  bot.sendMessage(fromId, "Daddy, I'm sleeping, will deploy in 5 mins")
+})
 
-bot.onText(/\/server (.+)/, function (msg, match) {
-  console.log(msg)
-  //var fromId = msg.from.id;
-  var fromId = msg.chat.id;
-  var deployCmd = match[1];
-  console.log(msg)
+bot.onText(/\/server (.+)/, (msg, match) => {
+  //const fromId = msg.from.id;
+  const fromId = msg.chat.id
+  const deployCmd = match[1]
 
-  var cmdMap = {
+  const cmdMap = {
     'mem': 'free',
     'disk': 'df',
     'mount': 'mount',
@@ -63,12 +55,9 @@ bot.onText(/\/server (.+)/, function (msg, match) {
 
   const child = exec(cmdMap[deployCmd],
            (error, stdout, stderr) => {
-             console.log(`stdout: ${stdout}`);
-             console.log(`stderr: ${stderr}`);
              if (error !== null) {
-               console.log(`exec error: ${error}`);
+               console.log(`exec error: ${error}`)
              }
-             bot.sendMessage(fromId, `Daddy, server ${deployCmd}. \n ${stderr} ${stdout}`);
+             bot.sendMessage(fromId, `Daddy, server ${deployCmd}. \n ${stderr} ${stdout}`)
            })
-});
-
+})
